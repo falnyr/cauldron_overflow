d31404cefe03d456a52d088849f0c7608536362a
Jan Richter <falnyr@gmail.com>
2021-05-08 13:23:59 +1200

    feat: chapter 18

--- assets/app.js
+++ assets/app.js
@@ -10,3 +10,18 @@ import './styles/app.css';
 
 // start the Stimulus application
 import './bootstrap';
+
+import $ from 'jquery';
+
+const container = $('.js-vote-arrows');
+container.find('a').on('click', e => {
+  e.preventDefault()
+  const link = $(e.currentTarget)
+
+  $.ajax({
+    url: '/comments/10/vote/'+link.data('direction'),
+    method: 'POST'
+  }).then(data => {
+    container.find('.js-vote-total').text(data.votes)
+  })
+})
